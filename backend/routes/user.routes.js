3c7297097eb8753e2ec96d5cc45de3abeab3e144
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-03-15 15:21:41 +0530

    user login and authuser middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,22 @@
 import { Router } from 'express';
 import *as userController from '../controllers/user.controller.js';
 import { body } from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.post('/register',
     body('email').isEmail().withMessage('Please enter a valid email address'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    userController.createUserController);
+    userController.createUserController
+);
+
+router.post('/Login',
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    userController.loginUserController
+);
+
+router.get('/profile', authMiddleware.authUser, userController.profileController);
 
 export default router;
