1c27c984350f9068ac2484796e80574931de60ee
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-03-18 16:35:53 +0530

    add getProjectById functionality in project controller and service, and update routes

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -59,4 +59,21 @@ export const addUserToProject = async (req, res) => {
         console.log(err);
         res.status(400).json({error:err.message});
     }
+};
+
+export const getProjectById = async (req, res) => {
+    const { projectId } = req.params;
+    try {
+
+        const project = await projectService.getProjectById({ projectId });
+
+        return res.status(200).json({
+            project
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({ error: err.message })
+    }
+
 }
--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -24,4 +24,9 @@ router.put('/add-user',
     projectController.addUserToProject
 )
 
+router.get('/get-project/:projectId',
+    authMiddleWare.authUser,
+    projectController.getProjectById
+)
+
 export default router;
