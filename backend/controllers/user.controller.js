a3a8e4d35bac13fc0e3f34c23af1d390169c7df0
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-03-18 16:14:43 +0530

    add project and user controllers for retrieving all projects and users, and implement user addition to projects

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,3 +77,18 @@ export const logoutController = async (req, res) => {
         res.status(400).send(err.message);
     }
 }
+
+export const getAllUsersController = async (req, res) => {
+    try{
+        const loggedInUser = await userModel.findOne({ 
+            email: req.user.email
+        });
+        const userId = loggedInUser._id;
+        const allUsers = await userService.getAllUsers({ userId });
+        res.status(200).json({ users: allUsers });
+
+    } catch(err){
+        console.log(err);
+        res.status(400).json({error:err.message});
+    }
+};
--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import projectModel from "../models/project.model.js";
 
 export const createProject = async ({name , userId}) => {
@@ -21,3 +22,67 @@ export const createProject = async ({name , userId}) => {
     }
     return project;
 }
+
+export const getAllProjectByUserId = async ({ userId }) => {
+    if (!userId) {
+        throw new Error('UserId is required')
+    }
+    const allUserProjects = await projectModel.find({
+        users: userId
+    })
+    return allUserProjects
+}
+
+export const addUsersToProject = async ({ projectId, users, userId }) => {
+    if(!projectId){
+        throw new Error('Project ID is required')
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(projectId)){
+        throw new Error('Invalid Project ID')
+    }
+
+    if(!users || users.length === 0){
+        throw new Error('Users are required')
+    }
+
+    if(!Array.isArray(users) || users.some(user => typeof user !== 'string')){
+        throw new Error('Users must be an array')
+    }
+
+    if(!userId){
+        throw new Error('User is required')
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        throw new Error('Invalid User ID')
+    }
+
+    const project = await projectModel.findOne({
+        _id: projectId,
+        users: userId
+    });
+
+    if(!project){
+        throw new Error('User not belong to this project')
+    }
+
+    const updatedProject = await projectModel.findOneAndUpdate
+    (
+        {
+            _id: projectId
+        },
+        {
+            $addToSet: {
+                users: {
+                    $each: users
+                }
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+    return updatedProject;
+};
