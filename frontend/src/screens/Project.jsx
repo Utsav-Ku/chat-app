e339bfa1d91e13887cc16f0b2ddd93810b892845
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-03-19 15:00:15 +0530

    creating project and messaging ui

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -7,7 +7,35 @@ const Project = () => {
     console.log(location.state)
 
     return (
-        <div>Project</div>
+        <main className='h-screen w-screen flex'>
+            <section className='left flex flex-col h-full min-w-72 bg-slate-300'>
+                <header className="flex justify-end p-2 px-4  w-full bg-slate-100" >
+                    <button className='p-2 '>
+                        <i className="ri-group-fill"></i>
+                    </button>
+                </header>
+
+                <div className="conversation-area flex-grow flex flex-col p-1 ">
+                    <div className="message-box flex-grow flex flex-col gap-1">
+                        <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small className='opacity-65 text-xm'>example@gmail.com</small>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div className="ml-auto max-w-56 message flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small className='opacity-65 text-xm'>example@gmail.com</small>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                    <div className="inputfield w-full flex">
+                        <input type='text' placeholder='Enter mesaage' className='p-2 px-4 border-none outline-none' ></input>
+                        <button className='flex-grow px-3'><i className="ri-send-plane-fill"></i></button>
+                    </div>
+                </div>
+
+
+            </section>
+
+        </main>
     )
 }
 
