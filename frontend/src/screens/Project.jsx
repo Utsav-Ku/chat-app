f35e7819ca7f3093aeb93cb591939c9b56839e27
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-03-21 22:17:20 +0530

    refactor messaging system; improve message handling and UI updates in Project component

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { UserContext } from '../context/user.context'
 import { useNavigate, useLocation } from 'react-router-dom'
 import axios from '../config/axios'
-import { initializeSocket,receiveMessage,sendMessage } from '../config/socket'
-import { UserContext } from '../context/user.context'
-
+import { initializeSocket, receiveMessage, sendMessage } from '../config/socket'
 
 const Project = () => {
 
@@ -13,9 +12,11 @@ const Project = () => {
     const [ isModalOpen, setIsModalOpen ] = useState(false)
     const [ selectedUserId, setSelectedUserId ] = useState([])
     const [ project, setProject ] = useState(location.state.project)
-    const [ users, setUsers ] = useState([])
-    const [message, setMessage] = useState('')
+    const [ message, setMessage ] = useState('')
     const { user } = useContext(UserContext)
+    const messageBox = React.createRef()
+
+    const [ users, setUsers ] = useState([])
 
     const handleUserClick = (id) => {
         setSelectedUserId(prevSelectedUserId => {
@@ -25,38 +26,41 @@ const Project = () => {
             } else {
                 newSelectedUserId.add(id);
             }
-            //console.log(Array.from(newSelectedUserId))
             return newSelectedUserId;
         });
     }
 
+
     function addCollaborators() {
-        axios.put('/projects/add-user',{
+        axios.put("/projects/add-user", {
             projectId: location.state.project._id,
             users: Array.from(selectedUserId)
-        }).then(res => {        
+        }).then(res => {
             console.log(res.data)
             setIsModalOpen(false)
-        })
-        .catch(err => {
-            console.log(err)    
+
+        }).catch(err => {
+            console.log(err)
         })
     }
 
-    function send() {
+    const send = () => {
         sendMessage('project-message', {
             message,
-            sender: user._id
+            sender: user
         })
-        setMessage('')
+        appendOutgoingMessage(message)
+        setMessage("")
     }
 
     useEffect(() => {
         initializeSocket(project._id)
         receiveMessage('project-message', data => {
             console.log(data)
+            appendIncomingMessage(data)
         })
         axios.get(`/projects/get-project/${location.state.project._id}`).then(res => {
+            console.log(res.data.project)
             setProject(res.data.project)
         })
         axios.get('/users/all').then(res => {
@@ -65,12 +69,42 @@ const Project = () => {
         .catch(err => {
             console.log(err)
         })
+
     }, [])
+
+
+    function appendIncomingMessage(messageObject) {
+        const messageBox = document.querySelector('.message-box')
+        const message = document.createElement('div')
+        message.classList.add('message', 'max-w-56', 'flex', 'flex-col', 'p-2', 'bg-slate-50', 'w-fit', 'rounded-md')
+        message.innerHTML = `
+                <small class='opacity-65 text-xs'>${messageObject.sender.email}</small>
+                <p class='text-sm'>${messageObject.message}</p>
+            `
+        messageBox.appendChild(message)
+        scrollToBottom()
+    }
+
+    function appendOutgoingMessage(message) {
+        const messageBox = document.querySelector('.message-box')
+        const newMessage = document.createElement('div')
+        newMessage.classList.add('ml-auto', 'max-w-56', 'message', 'flex', 'flex-col', 'p-2', 'bg-slate-50', 'w-fit', 'rounded-md')
+        newMessage.innerHTML = `
+                    <small class='opacity-65 text-xs'>${user.email}</small>
+                    <p class='text-sm'>${message}</p>
+                `
+        messageBox.appendChild(newMessage)
+        scrollToBottom()
+    }
+
+    function scrollToBottom() {
+        messageBox.current.scrollTop = messageBox.current.scrollHeight
+    }
 
     return (
         <main className='h-screen w-screen flex'>
-            <section className="left relative flex flex-col h-full min-w-96 bg-slate-300">
-                <header className='flex justify-between items-center p-2 px-4 w-full bg-slate-100'>
+            <section className="left relative flex flex-col h-screen min-w-96 bg-slate-300">
+                <header className='flex justify-between items-center p-2 px-4 w-full bg-slate-100 absolute top-0'>
                     <button className='flex gap-2' onClick={() => setIsModalOpen(true)}>
                         <i className="ri-add-fill mr-1"></i>
                         <p>Add collaborator</p>
@@ -79,20 +113,16 @@ const Project = () => {
                         <i className="ri-group-fill"></i>
                     </button>
                 </header>
-                <div className="conversation-area flex-grow flex flex-col">
-                    <div className="message-box p-1 flex-grow flex flex-col gap-1">
-                        <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
-                            <small className='opacity-65 text-xs'>example@gmail.com</small>
-                            <p className='text-sm'>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="ml-auto max-w-56 message flex flex-col p-2 bg-slate-50 w-fit rounded-md">
-                            <small className='opacity-65 text-xs'>example@gmail.com</small>
-                            <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    </div>
-                    <div className="inputField w-full flex">
-                        <input value={message} onChange={(e) => setMessage(e.target.value) } className='p-2 px-4 border-none outline-none flex-grow' type="text" placeholder='Enter message' />
-                        <button onClick={send} className='px-5 bg-slate-950 text-white'><i className="ri-send-plane-fill"></i></button>
+                <div className="conversation-area pt-14 pb-10 flex-grow flex flex-col h-full relative">
+                    <div ref={messageBox} className="message-box p-1 flex-grow flex flex-col gap-1 overflow-auto max-h-full scrollbar-hide"></div>
+                    <div className="inputField w-full flex absolute bottom-0">
+                        <input
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            className='p-2 px-4 border-none outline-none flex-grow' type="text" placeholder='Enter message' />
+                        <button
+                            onClick={send}
+                            className='px-5 bg-slate-950 text-white'><i className="ri-send-plane-fill"></i></button>
                     </div>
                 </div>
                 <div className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'} top-0`}>
@@ -104,12 +134,12 @@ const Project = () => {
                     </header>
                     <div className="users flex flex-col gap-2">
                         {project.users && project.users.map(user => {
-                            return(
+                            return (
                                 <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
                                     <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
                                         <i className="ri-user-fill absolute"></i>
                                     </div>
-                                    <h1 className='font-semibold text-lg'>{user.email || 'username'}</h1>
+                                    <h1 className='font-semibold text-lg'>{user.email}</h1>
                                 </div>
                             )
                         })}
@@ -135,7 +165,9 @@ const Project = () => {
                                 </div>
                             ))}
                         </div>
-                        <button onClick={ addCollaborators } className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-600 text-white rounded-md'>
+                        <button
+                            onClick={addCollaborators}
+                            className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-600 text-white rounded-md'>
                             Add Collaborators
                         </button>
                     </div>
